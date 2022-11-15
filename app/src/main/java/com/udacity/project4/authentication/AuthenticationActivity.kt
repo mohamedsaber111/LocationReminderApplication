03cfa175de79d09ea5e89bd56bab02e27ac8f0bf
mohamed saber <eng.muhammedsaber@gmail.com>
2022-11-15 16:25:59 +0200

    observe login when success go to reminderActivity and do logout in reminderListFragment

--- app/src/main/java/com/udacity/project4/authentication/AuthenticationActivity.kt
+++ app/src/main/java/com/udacity/project4/authentication/AuthenticationActivity.kt
@@ -1,11 +1,15 @@
 package com.udacity.project4.authentication
 
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.observe
 import com.firebase.ui.auth.AuthUI
 import com.udacity.project4.R
 import com.udacity.project4.databinding.ActivityAuthenticationBinding
+import com.udacity.project4.locationreminders.RemindersActivity
 
 /**
  * This class should be the starting point of the app, It asks the users to sign in / register, and redirects the
@@ -15,7 +19,7 @@ class AuthenticationActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityAuthenticationBinding
 
-
+    private val loginViewModel by viewModels<AuthenticationViewModel>()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityAuthenticationBinding.inflate(layoutInflater)
@@ -25,6 +29,17 @@ class AuthenticationActivity : AppCompatActivity() {
 
         binding.login.setOnClickListener { launchToSignInFlow() }
 
+        loginViewModel.authenticationState.observe(this) { authState ->
+            when (authState) {
+                //if AUTHENTICATED go to reminderActivity
+                AuthenticationViewModel.AuthState.IS_AUTHENTICATED -> navigation()
+                //if it is UNAUTHENTICATED show message that , sign in is unSuccessful
+                else -> Log.e(
+                    "login",
+                    "Auth state doesn't require user interface change $authState"
+                )
+            }
+        }
 
     }
 
@@ -47,5 +62,9 @@ class AuthenticationActivity : AppCompatActivity() {
 
     companion object {
         const val SIGN_IN_RESULT_CODE_IS = 10
+    }
+    private fun navigation() {
+        val navigationIntent = Intent(this, RemindersActivity::class.java)
+        startActivity(navigationIntent)
     }
 }
